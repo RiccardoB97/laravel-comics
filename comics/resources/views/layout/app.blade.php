0da1bf1bed4e6fbef468e1c2a271ec251e8500d2
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-08 17:53:47 +0200

    linked css stylesheet with asset()

--- comics/resources/views/layout/app.blade.php
+++ comics/resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Link style.css -->
-    <link rel="stylesheet" href="assets()">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/1d47f7a9f5.js" crossorigin="anonymous"></script>
     <title>@yield('title', 'Laravel-comics')</title>
