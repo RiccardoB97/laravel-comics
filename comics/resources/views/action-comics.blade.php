9ae2297912471d95efeaaf19c42f4beaa73585f0
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-08 17:07:42 +0200

    used yield and extends to create page template

---
+++ comics/resources/views/action-comics.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('title', 'Action Comics - Laravel-comics')
+
+@section('content')
+    <h1>Action</h1>
+@endsection
--- comics/resources/views/comics.blade.php
+++ comics/resources/views/comics.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Laravel-comics</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layout.app')
+
+@section('title', 'Home - Laravel-comics')
+
+@section('content')
+    <h1>Main content</h1>
+@endsection
