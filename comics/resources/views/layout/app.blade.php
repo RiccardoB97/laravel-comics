b8d0af90bd57e24e9870a0e86147df941d0cd682
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-08 19:24:00 +0200

    Add top side of footer with link lists

--- comics/resources/views/layout/app.blade.php
+++ comics/resources/views/layout/app.blade.php
@@ -53,7 +53,63 @@
     <!-- Site footer -->
     <footer id="site_footer">
         <div class="links">
+            <div class="container">
+                <div class="link_cols">
+                    <div class="col">
+                        <h2>DC COMICS</h2>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
 
+                        <h2>SHOP</h2>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h2>DC</h2>
+                        <ul>
+                            <li><a href="#">Terms of use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshop</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h2>SITES</h2>
+                        <ul>
+                            <li>
+                                <a href="#">DC</a>
+                                <a href="#">MAD Magazine</a>
+                                <a href="#">DC Kids</a>
+                                <a href="#">DC Universe</a>
+                                <a href="#">DC Power Visa</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="logo_bg">
+                    
+                </div>
+                <div class="rights">
+                    <p>All site content TM and <i class="far fa-copyright"></i> 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>  </span>
+                    <p><a href="#">Cookies settings</a></p>
+                </div>
+            </div>
         </div>
         <div class="socials">
             <div class="container">
