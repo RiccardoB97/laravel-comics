41220336d97249280c521216bb7d924a5bf85d8a
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-08 21:03:57 +0200

    Add shop section in comics

--- comics/resources/views/comics.blade.php
+++ comics/resources/views/comics.blade.php
@@ -17,5 +17,29 @@
             </div>
             @endforeach
         </div>
+        <div class="load">
+            <a href="#">LOAD MORE</a>
+        </div>
+    </section>
+    <section class="shop-wrap">
+        <div class="container">
+            <div class="shop">
+                <div class="buy">
+                    <img src="../images/buy-comics-digital-comics.png" alt=""> <span>DIGITAL COMICS</span>
+                </div>
+                <div class="buy">
+                    <img src="../images/buy-comics-merchandise.png" alt=""> <span>DC MERCHANDISE</span>
+                </div>
+                <div class="buy">
+                    <img src="../images/buy-comics-subscriptions.png" alt=""> <span>SUBSCRIPTION</span>
+                </div>
+                <div class="buy">
+                    <img src="../images/buy-comics-shop-locator.png" alt=""> <span>COMIC SHOP LOCATOR</span>
+                </div>
+                <div class="buy">
+                    <img src="../images/buy-dc-power-visa.svg" alt=""> <span>DC POWER VISA</span>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
